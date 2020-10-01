0afd05d26203c6d1c5c7d80d9153bb4efed8d06f
birgit <birgit.ast@gmail.com>
2020-10-01 03:17:35 +0200

    added weight initializations

--- models.py
+++ models.py
@@ -6,6 +6,8 @@ from torchmeta.modules import (MetaModule, MetaConv2d, MetaBatchNorm2d,
                                MetaSequential, MetaLinear)
 from collections import OrderedDict
 
+import math
+
 
 # simple UNet implementation
 class Unet(MetaModule):
@@ -43,10 +45,21 @@ class Unet(MetaModule):
         self.up_concat1 = unetUp(filters[1], filters[0], self.is_deconv)
 
         # final conv (without any concat)
-        self.final = nn.Conv2d(filters[0], n_classes, 1)
+        self.final = MetaConv2d(filters[0], n_classes, kernel_size=1)
 
-    def forward(self, inputs, params):
-        #print("model forward")
+
+        """for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    print("conv")
+                    print(m)
+                    nn.init.xavier_uniform(m.weight)
+                    nn.init.xavier_uniform(m.bias)
+                elif isinstance(m, nn.BatchNorm2d):
+                    print("batch")
+                    m.weight.data.fill_(1)
+                    m.bias.data.zero_()"""
+
+    def forward(self, inputs, params=None):
         conv1 = self.conv1(inputs)
         maxpool1 = self.maxpool1(conv1)
 
@@ -67,12 +80,13 @@ class Unet(MetaModule):
         up1 = self.up_concat1(conv1, up2)
 
         final = self.final(up1)
+        #final = F.log_softmax(final)
+        final = torch.sigmoid(final)
 
-        for i in range(list(final.shape)[0]):
+        """for i in range(list(final.shape)[0]):
             prob_map = torch.sigmoid(final[i])
             #mask = prob_map > 0.5#seg_threshold
-            final[i] = prob_map
-        #print("end model forward")
+            final[i] = prob_map"""
 
         return final
 
@@ -80,10 +94,21 @@ class Unet(MetaModule):
 
 
 class unetConv2(MetaModule):
-    def __init__(self, in_channels, out_channels, is_batchnorm, kernel_size=3, stride=1, padding=1):
+    def __init__(self, in_channels, out_channels, is_batchnorm, kernel_size=3, stride=1, padding=1, final=False):
         super(unetConv2, self).__init__()
 
+        def init_layers(m):
+            if type(m) == MetaConv2d:
+                #torch.nn.init.xavier_uniform_(m.weight)
+                n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels # or out_channels like in https://discuss.pytorch.org/t/unet-implementation/426/23
+                torch.nn.init.normal_(m.weight, mean = 0.0, std = math.sqrt(2.0/n)) # from UNet Paper
+                #torch.nn.init.kaiming_uniform_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
         if is_batchnorm:
+            
             self.conv = MetaSequential(OrderedDict([
             ('conv', MetaConv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)),
             ('norm', nn.BatchNorm2d(out_channels)),# momentum=1.,track_running_stats=False)),
@@ -101,6 +126,8 @@ class unetConv2(MetaModule):
             ('relu', nn.ReLU())
             ]))
 
+        # use the modules apply function to recursively apply the initialization
+        self.conv.apply(init_layers)
 
     def forward(self, inputs):
         outputs = self.conv(inputs)
@@ -110,7 +137,8 @@ class unetConv2(MetaModule):
 class unetUp(MetaModule):
     def __init__(self, in_size, out_size, is_deconv):
         super(unetUp, self).__init__()
-        self.conv = unetConv2(in_size, out_size, False)
+
+        self.conv = unetConv2(in_size, out_size, is_batchnorm=False)
         if is_deconv:
             self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
         else:
